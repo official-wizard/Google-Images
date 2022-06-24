47c952fdab9601a9f9e317be17dd7711e3fbcaa4
Link <lenkgit@gmail.com>
2022-06-23 20:39:07 -0400

    ADD: Safe Search Support

--- src/main/java/sh/hyrule/google/images/GoogleImages.kt
+++ src/main/java/sh/hyrule/google/images/GoogleImages.kt
@@ -9,8 +9,8 @@ object GoogleImages {
     private const val userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
     private const val googleSearchQuery = "https://www.google.com/search"
 
-    fun search(query: String): List<String> {
-        return getImagesSrc(createConnection(query).get())
+    fun search(query: String, safeSearch: Boolean): List<String> {
+        return getImagesSrc(createConnection(query, safeSearch).get())
     }
 
     private fun getImagesSrc(document: Document): List<String> {
@@ -19,12 +19,18 @@ object GoogleImages {
             .map { it.attr("data-src") }.toList()
     }
 
-    private fun createConnection(query: String): Connection {
-        return Jsoup.connect(createQuery(query))
+    private fun createConnection(query: String, safeSearch: Boolean): Connection {
+        return Jsoup.connect(createQuery(query, safeSearch))
             .userAgent(userAgent)
     }
 
-    private fun createQuery(query: String): String {
-        return "$googleSearchQuery?q=${URLEncoder.encode(query, "UTF-8")}&tbm=isch"
+    private fun createQuery(query: String, safeSearch: Boolean): String {
+        var result = "$googleSearchQuery?q=${URLEncoder.encode(query, "UTF-8")}&tbm=isch"
+
+        if (safeSearch) {
+            result += "&safe=active"
+        }
+
+        return result
     }
 }
